4ad46e393124a9a8e3203452c0f529b0089ad498
Tom Kunc <tomkunc0@gmail.com>
2019-05-26 09:48:29 +1000

    Add line memory to mips_parser

--- src/mips_parser.rs
+++ src/mips_parser.rs
@@ -5,11 +5,19 @@ use super::utils::smart_split::SmartSplit;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct MIPSLocation {
+    file: String,
+    line_num: usize,
+    line_text: String
+}
+
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub struct MIPSDirective {
     directive_type: String,
-    directive_value: Vec<String>
+    directive_value: Vec<String>,
+    directive_location: MIPSLocation
 }
 
 #[derive(Debug)]
@@ -23,6 +31,7 @@ pub struct MIPSLabel {
 pub struct MIPSInstruction {
     instr_type: String,
     instr_args: Vec<String>,
+    instr_location: MIPSLocation
 } 
 
 
@@ -38,7 +47,7 @@ fn parse_line_to_parts<'a>(line: &'a String) -> impl Iterator<Item = &'a str> {
     SmartSplit::new(line)
 }
 
-fn parse_parts_to_component<'a>(parts: &mut impl Iterator<Item =&'a str>) -> Option<Vec<MIPSComponent>> {
+fn parse_parts_to_component<'a>(parts: &mut impl Iterator<Item =&'a str>, location: MIPSLocation) -> Option<Vec<MIPSComponent>> {
     let mut return_parts:Vec<MIPSComponent> = Vec::new();
     let possible_label = parts.next()?;
     if possible_label.chars().last().unwrap() == ':' {
@@ -46,7 +55,7 @@ fn parse_parts_to_component<'a>(parts: &mut impl Iterator<Item =&'a str>) -> Opt
             label: possible_label.to_owned()
         };
         return_parts.push(MIPSComponent::Label(new_label));
-        let recursed_parts = parse_parts_to_component(parts);
+        let recursed_parts = parse_parts_to_component(parts, location.clone());
         if let Some(recursed_parts) = recursed_parts {
             return_parts.extend(recursed_parts);
         }
@@ -57,13 +66,15 @@ fn parse_parts_to_component<'a>(parts: &mut impl Iterator<Item =&'a str>) -> Opt
             let directive_type_str: String = possible_directive.collect();
             let directive =  MIPSDirective {
                 directive_type: directive_type_str.to_owned(),
-                directive_value: parts.map(|x| x.to_owned()).collect()
+                directive_value: parts.map(|x| x.to_owned()).collect(),
+                directive_location: location
             };
             return_parts.push(MIPSComponent::Directive(directive));
         } else {
             let instruction = MIPSInstruction {
                 instr_type: possible_instruction.collect(),
-                instr_args: parts.map(|x| x.to_owned()).collect()
+                instr_args: parts.map(|x| x.to_owned()).collect(),
+                instr_location: location
             };
             return_parts.push(MIPSComponent::Instruction(instruction));
         }
@@ -72,20 +83,26 @@ fn parse_parts_to_component<'a>(parts: &mut impl Iterator<Item =&'a str>) -> Opt
     Some(return_parts)
 }
 
-fn parse_line_to_component(line: &String, ) -> Option<Vec<MIPSComponent>> {
+fn parse_line_to_component(line: &String, location: MIPSLocation) -> Option<Vec<MIPSComponent>> {
     let mut parts = parse_line_to_parts(line);
-    parse_parts_to_component(&mut parts)
+    parse_parts_to_component(&mut parts, location)
 
 }
+
 pub fn read_file_to_state<'a>(file_name: &String) -> Result<(Vec<MIPSComponent> , HashMap<String, &'a MIPSComponent>)> {
     let component_list = Vec::new();
     let label_map = HashMap::new();
 
 
     let file = File::open(file_name)?;
-    for line in BufReader::new(file).lines() {
+    for (i, line) in BufReader::new(file).lines().enumerate() {
         let mut line = line?.clone();
-        parse_line_to_component(&mut line);
+        let location = MIPSLocation {
+            file: file_name.to_owned(),
+            line_num: i,
+            line_text: line.clone()
+        };
+        parse_line_to_component(&mut line, location);
     }
 
     Ok((component_list, label_map))
@@ -117,8 +134,9 @@ mod tests {
 
     #[test]
     fn test_parse_line_to_parts_label() {
+        let loc = MIPSLocation {file: "".to_string(), line_num: 0, line_text: "".to_string()};
         compare_results(
-            parse_line_to_component(&"label: ".to_string()).unwrap(),
+            parse_line_to_component(&"label: ".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label:".to_string()
@@ -126,7 +144,7 @@ mod tests {
             ]
         );
         compare_results(
-            parse_line_to_component(&"label1: label2: ".to_string()).unwrap(),
+            parse_line_to_component(&"label1: label2: ".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label1:".to_string()
@@ -140,21 +158,24 @@ mod tests {
     
     #[test]
     fn test_parse_line_to_parts_directive() {
+        let loc = MIPSLocation {file: "".to_string(), line_num: 0, line_text: "".to_string()};
         compare_results(
-            parse_line_to_component(&".directive".to_string()).unwrap(),
+            parse_line_to_component(&".directive".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Directive(MIPSDirective {
                     directive_type: "directive".to_string(),
-                    directive_value: vec![]
+                    directive_value: vec![],
+                    directive_location: loc.clone()
                 }),
             ]
         );
         compare_results(
-            parse_line_to_component(&".directive arg1 arg2".to_string()).unwrap(),
+            parse_line_to_component(&".directive arg1 arg2".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Directive(MIPSDirective {
                     directive_type: "directive".to_string(),
-                    directive_value: vec!["arg1".to_string(), "arg2".to_string()]
+                    directive_value: vec!["arg1".to_string(), "arg2".to_string()],
+                    directive_location: loc.clone(),
                 }),
             ]
         );
@@ -162,39 +183,43 @@ mod tests {
     
     #[test]
     fn test_parse_line_to_parts_label_directive() {
+        let loc = MIPSLocation {file: "".to_string(), line_num: 0, line_text: "".to_string()};
         compare_results(
-            parse_line_to_component(&"label:    \t .directive".to_string()).unwrap(),
+            parse_line_to_component(&"label:    \t .directive".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label:".to_string()
                 }),
                 MIPSComponent::Directive(MIPSDirective {
                     directive_type: "directive".to_string(),
-                    directive_value: vec![]
+                    directive_value: vec![],
+                    directive_location: loc.clone()
                 }),
             ]
         );
         compare_results(
-            parse_line_to_component(&"label: .directive \targ1 \targ2".to_string()).unwrap(),
+            parse_line_to_component(&"label: .directive \targ1 \targ2".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label:".to_string()
                 }),
                 MIPSComponent::Directive(MIPSDirective {
                     directive_type: "directive".to_string(),
-                    directive_value: vec!["arg1".to_string(), "arg2".to_string()]
+                    directive_value: vec!["arg1".to_string(), "arg2".to_string()],
+                    directive_location: loc.clone()
                 }),
             ]
         );
         compare_results(
-            parse_line_to_component(&"label: .directive 'text text text'".to_string()).unwrap(),
+            parse_line_to_component(&"label: .directive 'text text text'".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label:".to_string()
                 }),
                 MIPSComponent::Directive(MIPSDirective {
                     directive_type: "directive".to_string(),
-                    directive_value: vec!["'text text text'".to_string()]
+                    directive_value: vec!["'text text text'".to_string()],
+                    directive_location: loc.clone()
                 }),
             ]
         );
@@ -202,21 +227,24 @@ mod tests {
     
     #[test]
     fn test_parse_line_to_parts_assembly() {
+        let loc = MIPSLocation {file: "".to_string(), line_num: 0, line_text: "".to_string()};
         compare_results(
-            parse_line_to_component(&"asm".to_string()).unwrap(),
+            parse_line_to_component(&"asm".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Instruction(MIPSInstruction {
                     instr_type: "asm".to_string(),
-                    instr_args: vec![]
+                    instr_args: vec![],
+                    instr_location: loc.clone()
                 }),
             ]
         );
         compare_results(
-            parse_line_to_component(&"asm arg1 arg2".to_string()).unwrap(),
+            parse_line_to_component(&"asm arg1 arg2".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Instruction(MIPSInstruction {
                     instr_type: "asm".to_string(),
-                    instr_args: vec!["arg1".to_string(), "arg2".to_string()]
+                    instr_args: vec!["arg1".to_string(), "arg2".to_string()],
+                    instr_location: loc.clone()
                 }),
             ]
         );
@@ -224,20 +252,23 @@ mod tests {
     
     #[test]
     fn test_parse_line_to_parts_label_assembly() {
+        let loc = MIPSLocation {file: "".to_string(), line_num: 0, line_text: "".to_string()};
         compare_results(
-            parse_line_to_component(&"label:    \t asm".to_string()).unwrap(),
+            parse_line_to_component(&"label:    \t asm".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label:".to_string()
                 }),
                 MIPSComponent::Instruction(MIPSInstruction {
                     instr_type: "asm".to_string(),
-                    instr_args: vec![]
+                    instr_args: vec![],
+                    instr_location: loc.clone()
+                        
                 }),
             ]
         );
         compare_results(
-            parse_line_to_component(&"label1: label2: asm \targ1 \targ2".to_string()).unwrap(),
+            parse_line_to_component(&"label1: label2: asm \targ1 \targ2".to_string(), loc.clone()).unwrap(),
             vec![
                 MIPSComponent::Label(MIPSLabel {
                     label: "label1:".to_string()
@@ -247,10 +278,10 @@ mod tests {
                 }),
                 MIPSComponent::Instruction(MIPSInstruction {
                     instr_type: "asm".to_string(),
-                    instr_args: vec!["arg1".to_string(), "arg2".to_string()]
+                    instr_args: vec!["arg1".to_string(), "arg2".to_string()],
+                    instr_location: loc.clone()
                 }),
             ]
         );
     }
-
 }
